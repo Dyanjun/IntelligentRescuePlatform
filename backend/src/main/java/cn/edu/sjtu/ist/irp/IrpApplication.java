477569f94fb4a9e82aefc5220be3600ca60c81a6
Dyanjun <294720181@qq.com>
2021-12-13 16:01:00 +0800

    init

---
+++ backend/src/main/java/cn/edu/sjtu/ist/irp/IrpApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sjtu.ist.irp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrpApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cn/edu/sjtu/ist/irp/IrpApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.sjtu.ist.irp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
